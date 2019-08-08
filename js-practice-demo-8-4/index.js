9a6a1e2a627b9b442ce5fe35fce44a45bd8ecc30
JunKingWang <1243402013@qq.com>
2019-08-08 22:10:21 +0800

    edit monster game & edd redMonster

--- js-practice-demo-8-4/index.js
+++ js-practice-demo-8-4/index.js
@@ -30,8 +30,8 @@
 	}
 
 	// class Role 生成角色
-	function Role ({img, context, imgPos, rect}) {
-		this.img =  img,
+	function Role ({img, context, imgPos, rect} = {}) {
+		this.img = img,
 		this.context = context,
 		this.imgPos = imgPos,
 		this.rect = rect;
@@ -66,54 +66,51 @@
 
 	// class Hero
 	function Hero (img, context) {
-		var imgPos = {
+		Role.call(this)
+		this.img = img;
+		this.context = context;
+		this.imgPos = {
 			x: 0,
 			y: 0,
 			width: 32,
 			height: 32
 		};
-		var rect = {
+		this.rect = {
 			x: 0,
 			y: 0,
 			width: 40,
 			height: 40
 		};
-		Role.call(this, {
-			img: img,
-			context: context,
-			imgPos: imgPos,
-			rect: rect
-		})
 	}
 	Hero.prototype = Object.create(Role.prototype);
 	Hero.prototype.moveAttach = function() {
 		document.onkeydown = () => {
 			var e = event || window.event;
 			// up 38  down 40  left 37  right 39
-			// 不要碰到怪兽
+			// 不要碰到怪兽 碰到再向前进就是攻击，
 			var removeFlag = true;
 			monsterArray.forEach((item, i) => {
-				var itemXMin = item.rect.x - item.rect.width
+				var itemXMin = item.rect.x - this.rect.width
 				var itemXMax = item.rect.x + item.rect.width
-				var itemYMin = item.rect.y - item.rect.height
+				var itemYMin = item.rect.y - this.rect.height
 				var itemYMax = item.rect.y + item.rect.height
 				if(e.keyCode == 38) {
-					if(this.rect.x > itemXMin && this.rect.x < itemXMax && this.rect.y - 20 < itemYMax && this.rect.y - 20 > itemYMin) {
+					if(this.rect.x > itemXMin && this.rect.x < itemXMax && this.rect.y == itemYMax) {
 						removeFlag = false
 						monsterArray[i].getAttached()
 					}
 				} else if(e.keyCode == 40) {
-					if(this.rect.x > itemXMin && this.rect.x < itemXMax && this.rect.y + 20 < itemYMax && this.rect.y + 20 > itemYMin) {
+					if(this.rect.x > itemXMin && this.rect.x < itemXMax && this.rect.y == itemYMin) {
 						removeFlag = false
 						monsterArray[i].getAttached()
 					}
 				} else if(e.keyCode == 37) {
-					if(this.rect.y > itemYMin && this.rect.y < itemYMax && this.rect.x - 20 < itemXMax && this.rect.x - 20 > itemXMin) {
+					if(this.rect.y > itemYMin && this.rect.y < itemYMax && this.rect.x == itemXMax) {
 						removeFlag = false
 						monsterArray[i].getAttached()
 					}
 				} else if(e.keyCode == 39) {
-					if(this.rect.y > itemYMin && this.rect.y < itemYMax && this.rect.x + 20 < itemXMax && this.rect.x + 20 > itemXMin) {
+					if(this.rect.y > itemYMin && this.rect.y < itemYMax && this.rect.x == itemXMin) {
 						removeFlag = false
 						monsterArray[i].getAttached()
 					}
@@ -122,13 +119,13 @@
 			if(removeFlag) {
 				this.clear()
 				if(e.keyCode == 38) {
-					this.rect.y = this.rect.y - 20 > 0 ? this.rect.y -= 20 : 0;
+					this.rect.y = this.rect.y - 10 > 0 ? this.rect.y -= 10 : 0;
 				} else if(e.keyCode == 40) {
-					this.rect.y = this.rect.y + 20 > 260 ? 260 : this.rect.y += 20;
+					this.rect.y = this.rect.y + 10 > 260 ? 260 : this.rect.y += 10;
 				} else if(e.keyCode == 37) {
-					this.rect.x = this.rect.x - 20 > 0 ? this.rect.x -= 20 : 0;
+					this.rect.x = this.rect.x - 10 > 0 ? this.rect.x -= 10 : 0;
 				} else if(e.keyCode == 39) {
-					this.rect.x = this.rect.x + 20 > 460 ? 460 : this.rect.x += 20;
+					this.rect.x = this.rect.x + 10 > 460 ? 460 : this.rect.x += 10;
 				}
 				this.draw();
 			}
@@ -137,43 +134,45 @@
 
 	// class Monster
 	function Monster (img, context, rect, name) {
+		Role.call(this)
 		this.blood = 100;
 		this.name = name;
-		var imgPos = {
+		this.img = img;
+		this.context = context;
+		this.imgPos = {
 			x: 858,
 			y: 529,
 			width: 32,
 			height: 32
 		};
-		var rect = {
+		this.rect = {
 			x: rect.x,
 			y: rect.y,
 			width: 40,
 			height: 40
 		};
-		Role.call(this, {
-			img: img,
-			context: context,
-			imgPos: imgPos,
-			rect: rect
-		})
 	}
 	Monster.prototype = Object.create(Role.prototype)
-
-	Monster.prototype.getAttached = function(i) {
+	Monster.prototype.getAttached = function() {
 		this.blood -= 10;
 		if(this.blood <= 0) {
 			this.clear();
 			this.blood = 0;
-			monsterArray.splice(i, 1)
+			monsterArray = monsterArray.filter((item) => {
+				return item.name !== this.name
+			})
 		}
 		document.getElementsByClassName(this.name)[0].innerText = this.blood;
+		if(monsterArray.length < 1) {
+			setTimeout(function() {
+				alert('HERO WIN！！')
+			})
+		}
 	}
-
-	Monster.prototype.addBloodBoard = function(i) {
+	Monster.prototype.addBloodBoard = function() {
 		var divHtml = document.createElement('div')
 		var nameHtml = document.createElement('span')
-		nameHtml.append(`${this.name} ${i} Blood: `)
+		nameHtml.append(`${this.name} Blood: `)
 		var bloodHtml = document.createElement('span')
 		bloodHtml.append(`${this.blood}`)
 		bloodHtml.setAttribute('class', this.name)
@@ -181,6 +180,19 @@
 		divHtml.append(bloodHtml)
 		document.getElementById('bloodBoard').append(divHtml);
 	}
+
+	// 红衣怪物 大boss 血厚
+	function RedMonster (img, context, rect, name) {
+		Monster.call(this, img, context, rect, name);
+		this.imgPos = {
+			x: 858,
+			y: 462,
+			width: 32,
+			height: 32
+		};
+		this.blood = 200;
+	}
+	RedMonster.prototype = Object.create(Monster.prototype)
 
 	function drawRole (context, heroImg, allSpriteImg) {
 		var monsterPositonArray = [{
@@ -193,17 +205,22 @@
 			rect: {
 				x: 100, y: 100
 			}
-		}, {
+		},  {
 			name: 'monster3',
 			rect: {
-				x: 140, y: 140
+				x: 190, y: 160
 			}
 		}]
 		monsterPositonArray.forEach((item, i) => {
 			var monster = new Monster(allSpriteImg, context, item.rect, item.name);
-			monster.draw().addBloodBoard(i);
+			monster.draw().addBloodBoard();
 			monsterArray.push(monster);
 		})
+
+		var redMonster = new RedMonster(allSpriteImg, context, {x: 140, y: 140}, 'redMonster');
+		redMonster.draw().addBloodBoard();
+		monsterArray.push(redMonster);
+
 		var hero = new Hero(heroImg, context);
 		hero.draw().moveAttach();
 	}
